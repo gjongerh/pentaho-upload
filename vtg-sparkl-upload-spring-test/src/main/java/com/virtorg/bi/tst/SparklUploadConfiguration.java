211bf9e294a259b91db82b34785e60f3fd0e5fa0
Gerard Jongerhuis <gerard@jongerhuis.nl>
2017-05-14 15:21:32 +0200

    shutdown the app with a Spring TaskSchedular

---
+++ vtg-sparkl-upload-spring-test/src/main/java/com/virtorg/bi/tst/SparklUploadConfiguration.java
@@ -0,0 +1,19 @@
+package com.virtorg.bi.tst;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class SparklUploadConfiguration {
+
+	@Bean(name = "taskScheduler")
+	public TaskScheduler taskSchedular() {
+		ThreadPoolTaskScheduler taskSchedular = new ThreadPoolTaskScheduler();
+		taskSchedular.setPoolSize(5);
+		return (TaskScheduler) taskSchedular;
+	}
+}
--- vtg-sparkl-upload-spring-test/src/main/java/com/virtorg/bi/tst/SparklUploadTestApp.java
+++ vtg-sparkl-upload-spring-test/src/main/java/com/virtorg/bi/tst/SparklUploadTestApp.java
@@ -1,8 +1,12 @@
 package com.virtorg.bi.tst;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +16,32 @@ public class SparklUploadTestApp {
 
     private static SpringApplication app;
     private static ConfigurableApplicationContext ctx;
+    @Autowired
+    private TaskScheduler taskScheduler;
     
-	@GetMapping("/")
+    /*
+     * ShutdownProgram runnable class
+     */
+    private class ShutdownProgram implements Runnable {
+        public void run() {
+            System.out.println("Starting shutdown the App...");
+            ctx.close();
+        }
+    }
+
+    /*
+     * Servlet mappings
+     */
+    @GetMapping("/")
     public String home() {
         return "Testing the Sparkl Upload App...";
     }
 
     @GetMapping("/shutdown")
-    public void shutdown() {
-    	//SpringApplication.exit(ctx, () -> 0);
-    	ctx.close();
+    public String shutdown() {
+    	Date date = new Date();
+    	taskScheduler.scheduleWithFixedDelay(new ShutdownProgram(), date.getTime()+1000);	// start in 1 sec (1000ms)
+    	return "Shutdown will be initiated over 1 sec...";
     }
     
     public static void main(String[] args) {
