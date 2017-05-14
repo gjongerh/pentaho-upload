02207974d1a8a6157bd13bd5e5cc7c13d099626f
Gerard Jongerhuis <gerard@jongerhuis.nl>
2017-05-14 14:52:37 +0200

    programmatically shutdown of the server

--- vtg-sparkl-upload-spring-test/src/main/java/com/virtorg/bi/tst/SparklUploadTestApp.java
+++ vtg-sparkl-upload-spring-test/src/main/java/com/virtorg/bi/tst/SparklUploadTestApp.java
@@ -2,6 +2,7 @@ package com.virtorg.bi.tst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SparklUploadTestApp {
 
-    @GetMapping("/")
+    private static SpringApplication app;
+    private static ConfigurableApplicationContext ctx;
+    
+	@GetMapping("/")
     public String home() {
         return "Testing the Sparkl Upload App...";
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(SparklUploadTestApp.class, args);
+    @GetMapping("/shutdown")
+    public void shutdown() {
+    	//SpringApplication.exit(ctx, () -> 0);
+    	ctx.close();
     }
+    
+    public static void main(String[] args) {
+        //SpringApplication.run(SparklUploadTestApp.class, args);
+        app = new SpringApplication(SparklUploadTestApp.class);
+        ctx = app.run(args);
+        System.out.print("done");
+   }
 }
