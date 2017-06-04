c1662c9ee2e9433c61da85a97bfe6a51f3e875aa
Gerard Jongerhuis <gerard@jongerhuis.nl>
2017-06-04 08:22:36 +0200

    working spring jersey 2.0 test TODO Pentaho is still Jersey 1.19.1

--- vtg-sparkl-upload-spring-test/src/test/java/com/virtorg/test/UploadJerseyTest.java
+++ vtg-sparkl-upload-spring-test/src/test/java/com/virtorg/test/UploadJerseyTest.java
@@ -2,8 +2,17 @@ package com.virtorg.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import javax.ws.rs.core.Application;
+import java.io.File;
+import java.io.IOException;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -24,9 +33,28 @@ public class UploadJerseyTest extends JerseyTest {
         return new JerseyConfig().property("contextConfig", context);
     }
 
+    @Override
+    protected void configureClient(ClientConfig config) {
+        config.register(MultiPartFeature.class);
+    }
+    
     @Test
     public void testHello() {
         final String hello = target("upload/hello").request().get(String.class);
         assertThat(hello).isEqualTo("Hello World");
+    }
+
+    @Test
+    public void testUpload() throws IOException {
+    	final FileDataBodyPart filePart = new FileDataBodyPart("file", new File("./work/eurotext.tif"));
+    	final FormDataMultiPart multipart = new FormDataMultiPart();
+    	multipart.field("endpointPath", "plugin/proef");
+    	multipart.bodyPart(filePart);
+    	
+    	final Response response = target("upload/file").request().post(Entity.entity(multipart, multipart.getMediaType()));
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.readEntity(String.class)).isEqualTo("File upload succesfull...");
+        
+        multipart.close();
     }
 }
